43fc844624617bacf90884e8daaa78e4b1cf6b30
Ofer Engel <o.engel@rug.nl>
2022-07-26 20:36:56 +0200

    Initial commit

---
+++ data-raw/weather/weather.R
@@ -0,0 +1,12 @@
+## code to prepare `weather` dataset goes here
+library(tidyverse)
+library(janitor)
+weather_raw <- read_csv(paste0(here::here(),
+                               "/data-raw/weather/weather.csv"),
+                        show_col_types=FALSE)
+weather <- weather_raw %>% clean_names()
+use_r("weather")
+devtools::document()
+
+usethis::use_data(weather, overwrite = TRUE)
+
